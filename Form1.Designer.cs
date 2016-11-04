a4d6d4d08f1303a98526042c779c937508268bb4
PE11 <pulon11ea6@gmail.com>
2016-11-04 17:03:21 +0900

    非同期処理Taskについて

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AsyncAwaitTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ProgressBar = new System.Windows.Forms.ProgressBar();
+			this.StartProcess = new System.Windows.Forms.Button();
+			this.CancelProcess = new System.Windows.Forms.Button();
+			this.DisplayMessage = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// ProgressBar
+			// 
+			this.ProgressBar.Location = new System.Drawing.Point(12, 22);
+			this.ProgressBar.Name = "ProgressBar";
+			this.ProgressBar.Size = new System.Drawing.Size(260, 23);
+			this.ProgressBar.TabIndex = 0;
+			// 
+			// StartProcess
+			// 
+			this.StartProcess.Location = new System.Drawing.Point(116, 96);
+			this.StartProcess.Name = "StartProcess";
+			this.StartProcess.Size = new System.Drawing.Size(75, 23);
+			this.StartProcess.TabIndex = 1;
+			this.StartProcess.Text = "処理開始";
+			this.StartProcess.UseVisualStyleBackColor = true;
+			this.StartProcess.Click += new System.EventHandler(this.StartProcess_Click);
+			// 
+			// CancelProcess
+			// 
+			this.CancelProcess.Location = new System.Drawing.Point(197, 96);
+			this.CancelProcess.Name = "CancelProcess";
+			this.CancelProcess.Size = new System.Drawing.Size(75, 23);
+			this.CancelProcess.TabIndex = 2;
+			this.CancelProcess.Text = "処理中止";
+			this.CancelProcess.UseVisualStyleBackColor = true;
+			this.CancelProcess.Click += new System.EventHandler(this.CancelProcess_Click);
+			// 
+			// DisplayMessage
+			// 
+			this.DisplayMessage.Location = new System.Drawing.Point(13, 52);
+			this.DisplayMessage.Name = "DisplayMessage";
+			this.DisplayMessage.Size = new System.Drawing.Size(259, 23);
+			this.DisplayMessage.TabIndex = 3;
+			this.DisplayMessage.Text = "hoge";
+			this.DisplayMessage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 135);
+			this.Controls.Add(this.DisplayMessage);
+			this.Controls.Add(this.CancelProcess);
+			this.Controls.Add(this.StartProcess);
+			this.Controls.Add(this.ProgressBar);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ProgressBar ProgressBar;
+		private System.Windows.Forms.Button StartProcess;
+		private System.Windows.Forms.Button CancelProcess;
+		private System.Windows.Forms.Label DisplayMessage;
+
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AsyncAwaitTest
+{
+	//Taskの参考サイト
+	//[URL]https://tocsworld.wordpress.com/2014/07/16/c-task%E3%81%AE%E3%82%AD%E3%82%BD
+
+	//非同期処理の種類について
+	//[URL]http://csharp.keicode.com/basic/async-intro.php
+
+	//C#の非同期処理を網羅的に
+	//[URL]http://blog.xin9le.net/entry/2012/07/27/091353
+
+	public partial class Form1 : Form
+	{
+		private const int MAXCOUNT = 1000;
+
+		private const int SLEEPTIME = 5;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			ProgressBar.Maximum = MAXCOUNT;
+		}
+
+		CancellationTokenSource cts;
+		TaskScheduler uiTaskScheduler;
+
+		/// <summary>
+		/// 処理開始
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void StartProcess_Click(object sender, EventArgs e)
+		{
+			StartProcess.Enabled = false;
+
+			//キャンセル時に必要
+			cts = new CancellationTokenSource();
+
+			//現在のスレッドのコンテキストを取得する(UIを生成したスレッド)
+			//生成されたコントロールは生成したスレッド上でしか操作出来ないためそれに利用する
+			uiTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+			//タスクを作成して開始
+			Task task = Task.Factory.StartNew(() => TargetTask(), cts.Token);
+
+			bool CancelFLG = false;
+
+			//キャンセル後の処理
+			cts.Token.Register(() => CancelTask(out CancelFLG));
+
+			//タスク完了後の処理
+			task.ContinueWith(_ => 
+			{
+				DisplayMessage.Text = CancelFLG == true ? "タスクを中止しました" : "タスクが完了しました";
+				StartProcess.Enabled = true;
+				if(!CancelFLG) this.Close();
+			}, uiTaskScheduler);
+			
+		}		
+
+		/// <summary>
+		/// 実行するタスク
+		/// </summary>
+		private void TargetTask()
+		{
+			for (int i = 1; i < MAXCOUNT; i++)
+			{
+				//UIの生成されたスレッドでタスクを実行する
+				Task ProgressbarUpdateTask = new Task(() => 
+				{
+					ProgressBar.Value = ProgressBar.Value + 1;
+					DisplayMessage.Text = i + "/" + MAXCOUNT;	
+				});
+				//開始
+				ProgressbarUpdateTask.Start(uiTaskScheduler);
+
+				//主動となる重い処理がキャンセルされた場合は処理を中止する
+				if (cts.IsCancellationRequested)
+				{
+					return;
+				}
+
+				//実行する処理
+				Thread.Sleep(SLEEPTIME);
+			}
+		}
+
+		/// <summary>
+		/// キャンセル時に実行するタスク
+		/// </summary>
+		private void CancelTask(out bool CancelFLG)
+		{
+			ProgressBar.Value = ProgressBar.Minimum;
+			DisplayMessage.Text = "処理をキャンセルしました";
+			CancelFLG = true;
+		}
+
+
+		/// <summary>
+		/// 処理中止
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CancelProcess_Click(object sender, EventArgs e)
+		{
+			cts.Cancel();
+		}
+	}
+}
